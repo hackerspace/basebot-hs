19ecef153e5d621c335bbf4126fa78812b2bf659
skirit <skirit@dump.cz>
2019-07-30 01:11:40 +0200

    connect to base48 and final changes

--- bot.hs
+++ bot.hs
@@ -11,15 +11,16 @@ import Control.Monad
 
 server = "irc.freenode.org"
 port   = 6667
-chan   = "#boths-testing"
-nick   = "botik"
-sfile  = "staf"
+chan   = "#base48"
+nick   = "basebot-hs"
+ofile  = "/sys/class/gpio/gpio1_pd0/value"
+cfile  = "/sys/class/gpio/gpio2_pd2/value"
 
 main = forever $ do
     h <- connectTo server (PortNumber (fromIntegral port))
     hSetBuffering h NoBuffering
     write h "NICK" nick
-    write h "USER" (nick++" 0 * :skirit's bot")
+    write h "USER" (nick++" 0 * :open switch bot")
     write h "JOIN" chan
     k <- newEmptyMVar
     bot k h 
@@ -36,7 +37,7 @@ listen k h = forever $ do
     putStrLn s
     eval k h s
 
-eval k h s | "!quit"  `isInfixOf`  s = write h "QUIT" ":Exiting" >> exitWith ExitSuccess
+--eval k h s | "!quit"  `isInfixOf`  s = write h "QUIT" ":Exiting" >> exitWith ExitSuccess
 eval k h s | "PING :" `isPrefixOf` s = write h "PONG" (':' : drop 6 s)
 eval k h s | "TOPIC " `isPrefixOf` (command s) || "332 " `isPrefixOf` (command s) = do
      tryPutMVar k (content s)
@@ -47,21 +48,24 @@ eval k h s | "TOPIC " `isPrefixOf` (command s) || "332 " `isPrefixOf` (command s
      content = drop 1 . dropWhile (/= ':') . drop 1
 eval _ _ _ = return () -- ignore everything else
 
-privmsg h s = write h "PRIVMSG" (chan ++ " :" ++ s)
+--privmsg h s = write h "PRIVMSG" (chan ++ " :" ++ s)
 
 bot k h = forkIO $ forever $ do
     threadDelay 1000000
-    f <- openFile sfile ReadMode
-    status <- hGetLine f
-    hClose f
+    fo <- openFile ofile ReadMode
+    fc <- openFile cfile ReadMode
+    so <- hGetLine fo
+    sc <- hGetLine fc
+    hClose fo
+    hClose fc
     m <- tryReadMVar k
     case m of
        Nothing -> return ()
        Just a -> do
-           if status == "open" && not ("base open " `isPrefixOf` a)
+           if so == "1" && sc == "0" && not ("base open " `isPrefixOf` a)
            then settopic h ("base open \\o/ " ++ (dropWhile (/= '|') a))
            else return ()
-           if status /= "open" && not ("base close " `isPrefixOf` a)
+           if so == "0" && sc == "1" && not ("base close " `isPrefixOf` a)
            then settopic h ("base close :( " ++ (dropWhile (/= '|') a))
            else return ()
 
